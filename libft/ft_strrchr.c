34b7eabd9eb79be132cd95e9ed160817d69f1923
Marc Fernandez Gracia <marcfer2@cbr2s4.42barcelona.com>
2023-05-03 18:35:15 +0200

    libft

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcfer2 <marcfer2@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 15:12:29 by marcfer2          #+#    #+#             */
+/*   Updated: 2023/05/03 15:12:37 by marcfer2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <libft.h>
+
+char *ft_strrchr (const char *str, int c)
+{
+	char *pt;
+	int len;
+
+	len = ft_strlen(str);
+
+	pt = (char *) str + len - 1;
+	if (c == '\0')
+	{
+		pt++;
+		return(pt);
+	}
+	while (len != 0)
+	{
+		if (*pt == c)
+		{
+			return (pt);
+		}
+		len--;
+		pt--;
+	}
+	return(NULL);
+}
---
+++ libft/libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcfer2 <marcfer2@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 18:25:32 by marcfer2          #+#    #+#             */
+/*   Updated: 2023/05/03 18:25:35 by marcfer2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+
+#include <stdio.h>
+#include <string.h>
+
+int ft_isalnum(int c);
+int ft_isalpha(int c);
+int ft_isascii(int c);
+int ft_isdigit(int c);
+int ft_isprint(int c);
+int ft_strncmp(const char *s1, const char *s2, unsigned int n);
+int ft_tolower(int c);
+int ft_toupper(int c);
+size_t ft_strlen(const char *s);
+char *ft_strchr(const char *str, int c);
+char *ft_strrchr(const char *str, int c);
+
+#endif

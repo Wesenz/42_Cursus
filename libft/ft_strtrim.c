79aefa69daecf3be57619831b120a67a2c67e163
wesen <marcfg.27@gmail.com>
2023-05-23 01:51:44 +0200

    added strtrim with mem leak

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -7,38 +7,34 @@
 char *ft_strtrim(char const *s1, char const *set)
 {   
     size_t  i;
-    size_t  j;
+    size_t  s1len;
+    size_t  newlen;
     char    *newstr;
     
+    if (!s1 || !set)
+        return (NULL);
     i = 0;
-    j = ft_strlen(s1) - 1;
-    newstr = (char *)malloc(sizeof(char *) * (j + 1));
+    s1len = ft_strlen(s1);
+    while (s1[i] && ft_strchr(set, s1[i]) != NULL) 
+        i++;
+    while (s1len > i && ft_strchr(set, s1[s1len]) != NULL) 
+        s1len--;
+    newlen = s1len - i + 1;
+    newstr = (char *)malloc(sizeof(char) * newlen + 1);
     if(!newstr)
         return (NULL);
-    while (i != j)
-    {
-        if (s1[i] == *set)//(s1[i] >= 9 && s1[i] <= 13) || s1[i] == 32)
-            i++;
-        newstr[i] = s1[i]; 
-        i++;
-    }
-    while (j != 0)
-    {
-        if (s1[j] == *set)
-            j--;
-        newstr[j] = s1[j];
-        j--;
-    }
-    newstr[i - j] = '\0';
+    ft_memcpy(newstr, s1 + i, newlen);
+    newstr[newlen] = '\0';
     return (newstr);
 }
 /*
 int main(void)
 {   
-    char    str[] = "1 2 3\n4 5 6\t7 8 9\0";
-    char    str1[] = "1 2 3 4 5 6 7 8 9";
+    char    str[] = "   1 2 3\n4 5 6\t7 8 9\0   ";
+    char    str1[] = "   1 2 3 4 5 6 7 8 9   ";
     char    set[] = " ";
 
     printf("%s\n", ft_strtrim(str, set));
+    printf("%s\n", ft_strtrim(str1, set));
     return (0);
 }*/

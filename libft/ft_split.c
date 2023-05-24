50635450af5ffeb8a6e66145a59a205dd46817bd
wesen <marcfg.27@gmail.com>
2023-05-24 18:45:19 +0200

    added half-done split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -1,11 +1,12 @@
 #include "libft.h"
 #include <stdio.h>
+#include <string.h>
 
 static int words(const char *s, char c)
 {
-    size_t          i;
-    unsigned int    trigger;
-    unsigned int    words;
+    int    i;
+    int    trigger;
+    int    words;
 
     i = 0; 
     trigger = 0;
@@ -26,53 +27,67 @@ static int words(const char *s, char c)
     return (words);
 }
 
-static  int word_size(const char *s, char c)
+static  char *cpy_letters(const char *s, int start, int end)
 {
-    size_t  size;
-    size_t  i;
+    char  *letters;
+    int  i;
 
     i = 0;
-    size = 0;
-    while (s[i] != '\0' && s[i] != c)
+    letters = malloc((end - start + 1) * sizeof(char));
+    while (start < end)
     {
-        size++;
+        letters[i++] = s[start++];
+
+    }
+    letters[i] = '\0';
+    return (letters);
+}
+
+char    **ft_split(char const *s, char c)
+{
+    int  i;
+    int  j;
+    int trigger;
+    int  slen;
+    char    **newstr;
+
+    i = 0;
+    j = 0;
+    trigger = -1;
+    slen = strlen(s);
+    newstr = malloc((words(s, c) + 1) * sizeof(char *));
+    if (!s || !newstr)
+        return (NULL);
+    while(i <= slen)
+    {
+        if(s[i] != c && trigger < 0)
+            trigger = i;
+        else if (s[i] == c || (trigger >= 0 && s[i] == '\0'))
+        {
+            newstr[j] = cpy_letters(s, trigger, i);
+            j++;
+            trigger = -1;
+        }
         i++;
     }
-    return (size);
-}
-
-char **ft_split(char const *s, char c)
-{
-    size_t  i;
-    size_t  j;
-    size_t  slen;
-    char    *newstr;
-
-    slen = strlen(s);
-    newstr = (char *)malloc(sizeof(char *) * (slen + 1));
-
-    while(i != slen)
-    {
-        if(s[i] == c)
-        {   
-            while(s[i] != c)
-            {
-                newstr[i - slen] = s[i];
-            }
-            i++;
-        }
-    }
-    newstr[i - slen] = '\0';
-
+    newstr[j] = '\0';
     return (newstr);
+    printf("Newstr: %s\n", *newstr);
 }
-
+/*
 int main(void)
 {
-    char    str[] = "spliteame esta string peluca";
-    char    c = " ";
-
-    printf("%s\n", ft_split(str, c));
-
-    return (0);
-}
+    char str[] = "spliteame esta string peluca";
+    char c = ' ';
+    char **result = ft_split(str, c);
+    if (result != NULL)
+    {
+        for (int i = 0; result[i] != NULL; i++)
+        {
+            printf("%s\n", result[i]);
+            free(result[i]);
+        }
+        free(result);
+    }
+    return 0;
+}*/
